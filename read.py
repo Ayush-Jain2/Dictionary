182569201c812920c68291d144fff518d8fe5675
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-05-23 23:04:30 +0530

    Added support for proper nouns

--- read.py
+++ read.py
@@ -2,9 +2,20 @@ import json
 from difflib import get_close_matches
 data=json.load(open("076 data.json","r"))
 
+def proper_noun(word):
+    temp=word.split(" ")
+    for i in range(len(temp)):
+        temp[i]=temp[i].capitalize()
+    w=" ".join(temp)
+    return w
+
 def translate(word):
     if word in data:
         return data[word]
+    elif word.capitalize() in data:
+        return data[word.capitalize()]
+    elif proper_noun(word) in data:
+        return data[proper_noun(word)]
     elif len(get_close_matches(word,data.keys(),cutoff=0.8))>0 :
         match=get_close_matches(word,data.keys(),cutoff=0.8)
         ans=input("Did you mean %s instead ?\nEnter Y for yes and N for no. \n" % match[0])
